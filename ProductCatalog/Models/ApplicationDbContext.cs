9093359db7f565b9fe1457a88e61fb6c52fe8aa5
Sakr Samir <eng.sakr888@gmail.com>
2021-03-08 18:36:58 +0200

    FirstConfiguration

---
+++ ProductCatalog/Models/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace ProductCatalog.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
